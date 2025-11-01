9cf141860be573c4fdbe63ffb3ea013aea9eb675
Sharipov Razil <sharipov.rr@phystech.edu>
2025-11-01 19:39:16 +0300

    add NettyHttpServer

---
+++ src/main/java/NettyHttpServer.java
@@ -0,0 +1,62 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class NettyHttpServer {
+
+  private final int port;
+
+  public NettyHttpServer(int port) {
+    this.port = port;
+  }
+
+  public void run() throws Exception {
+    EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+    EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+    try {
+      ServerBootstrap b = new ServerBootstrap();
+      b.group(bossGroup, workerGroup)
+          .channel(NioServerSocketChannel.class)
+          .handler(new LoggingHandler(LogLevel.INFO))
+          .childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel ch) {
+              ChannelPipeline p = ch.pipeline();
+
+              p.addLast(new HttpServerCodec());
+
+              p.addLast(new HttpObjectAggregator(65536));
+
+              p.addLast(new HttpRequestHandler());
+            }
+          })
+          .option(ChannelOption.SO_BACKLOG, 128)
+          .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+      ChannelFuture f = b.bind(port).sync();
+      System.out.println("Netty HTTP Server started on port " + port);
+
+      f.channel().closeFuture().sync();
+
+    } finally {
+      workerGroup.shutdownGracefully();
+      bossGroup.shutdownGracefully();
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    int port = 8080;
+    if (args.length > 0) {
+      port = Integer.parseInt(args[0]);
+    }
+
+    new NettyHttpServer(port).run();
+  }
+}
