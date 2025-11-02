f74ed5f4fd036caef5d0ffb6d62f6586b356294f
Nikita Krivov <niknike2007@mail.ru>
2025-11-02 12:26:56 +0300

    Реализован класс обработки простых http-запросов

---
+++ src/main/java/com/mipt/server/NettyHttpServer.java
@@ -0,0 +1,66 @@
+package com.mipt.server;
+
+import com.mipt.userstorage.service.CacheStorageService;
+import com.mipt.userstorage.dao.*;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class NettyHttpServer {
+  private final int port;
+  private final CacheStorageService cacheService;
+  private final UserDAO userDAO;
+  private final PermissionDAO permissionDAO;
+  private final CacheStorageDAO cacheStorageDAO;
+
+  public NettyHttpServer(int port,
+      CacheStorageService cacheService,
+      UserDAO userDAO,
+      PermissionDAO permissionDAO,
+      CacheStorageDAO cacheStorageDAO) {
+    this.port = port;
+    this.cacheService = cacheService;
+    this.userDAO = userDAO;
+    this.permissionDAO = permissionDAO;
+    this.cacheStorageDAO = cacheStorageDAO;
+  }
+
+  public void run() throws Exception {
+    EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+    EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+    try {
+      ServerBootstrap b = new ServerBootstrap();
+      b.group(bossGroup, workerGroup)
+          .channel(NioServerSocketChannel.class)
+          .handler(new LoggingHandler(LogLevel.INFO))
+          .childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel ch) {
+              ChannelPipeline p = ch.pipeline();
+
+              p.addLast(new HttpServerCodec());
+              p.addLast(new HttpObjectAggregator(65536));
+              p.addLast(new HttpRequestHandler(cacheService, userDAO, permissionDAO, cacheStorageDAO));
+            }
+          })
+          .option(ChannelOption.SO_BACKLOG, 128)
+          .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+      ChannelFuture f = b.bind(port).sync();
+      System.out.println("Netty HTTP Server started on port " + port);
+
+      f.channel().closeFuture().sync();
+
+    } finally {
+      workerGroup.shutdownGracefully();
+      bossGroup.shutdownGracefully();
+    }
+  }
+}
