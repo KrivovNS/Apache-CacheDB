ad04a47769ef12154424e187fc1867acf89ecf3d
Sharipov Razil <sharipov.rr@phystech.edu>
2025-12-05 20:58:17 +0300

    enum

---
+++ src/main/java/com/mipt/model/MaxMemoryPolicy.java
@@ -0,0 +1,44 @@
+package com.mipt.model;
+
+public enum MaxMemoryPolicy {
+  NOEVICTION("noeviction"),
+  ALLKEYSLRU("allkeys-lru"),
+  VOLATILELRU("volatile-lru");
+
+
+  private final String value;
+
+  MaxMemoryPolicy(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public static MaxMemoryPolicy fromString(String type) {
+    if (type == null) {
+      return null;
+    }
+
+    for (MaxMemoryPolicy MaxMemoryPolicy : MaxMemoryPolicy.values()) {
+      if (MaxMemoryPolicy.getValue().equals(type.toLowerCase())) {
+        return MaxMemoryPolicy;
+      }
+    }
+    return null;
+  }
+
+  public static boolean isValid(String type) {
+    return fromString(type) != null;
+  }
+
+  public static String[] getAllValues() {
+    MaxMemoryPolicy[] types = values();
+    String[] values = new String[types.length];
+    for (int i = 0; i < types.length; i++) {
+      values[i] = types[i].getValue();
+    }
+    return values;
+  }
+}
