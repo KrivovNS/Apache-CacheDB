4a1d26c8b67b2c34b39b1fdf67e6cd4932dc3daa
Sharipov Razil <sharipov.rr@phystech.edu>
2025-12-05 05:43:45 +0300

    добавлены методы для TTL

--- src/main/java/com/mipt/cache/Cache.java
+++ src/main/java/com/mipt/cache/Cache.java
@@ -3,11 +3,23 @@ package com.mipt.cache;
 import java.util.Set;
 
 public interface Cache {
-  void put(Object key, Object value);
+  void put(Object key, Object value, long size);
   Object get(Object key);
   void remove(Object key);
   void clear();
   int size();
+  long totalSizeInBytes();
   boolean containsKey(Object key);
   Set<Object> getKeys();
+
+  // Новые методы для TTL и управления памятью
+  default void putWithTTL(Object key, Object value, long size, Long ttlMillis) {
+    put(key, value, size);
+  }
+
+  default boolean isKeyExpired(Object key) {
+    return false;
+  }
+
+  default void cleanupExpired() {}
 }
