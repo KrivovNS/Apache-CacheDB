ba8f167b09ea335fd2415179f0ba68eea553fc27
Sharipov Razil <sharipov.rr@phystech.edu>
2025-12-07 01:23:43 +0300

    вынес в properties

--- src/main/java/com/mipt/service/CacheStorageService.java
+++ src/main/java/com/mipt/service/CacheStorageService.java
@@ -1,18 +1,17 @@
 package com.mipt.service;
 
-import static com.mipt.model.MaxMemoryPolicy.ALLKEYSLRU;
-
 import com.mipt.cache.CacheResult;
 import com.mipt.model.DataType;
 import com.mipt.model.CacheStorage;
 import com.mipt.model.MaxMemoryPolicy;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class CacheStorageService {
-
-  private static final long DEFAULT_MAX_MEMORY = 100 * 1024 * 1024; // 100MB
 
   private CacheStorage database;
   private final ScheduledExecutorService cleanupScheduler;
@@ -22,23 +21,29 @@ public class CacheStorageService {
   private boolean canPolicyBeChanged;
 
   public CacheStorageService() {
-    this(DEFAULT_MAX_MEMORY, ALLKEYSLRU);
-  }
+    try {
+      Properties props = loadProperties();
+      this.maxMemory = Long.parseLong(props.getProperty("cache.max.memory", "104857600"));
+      this.maxMemoryPolicy = parsePolicy(props, "cache.max.memory.policy",
+          MaxMemoryPolicy.ALLKEYSLRU);
+      this.canPolicyBeChanged = true;
 
-  public CacheStorageService(long maxMemory, MaxMemoryPolicy maxMemoryPolicy) {
-    this.maxMemory = maxMemory;
-    this.maxMemoryPolicy = maxMemoryPolicy;
+    } catch (IOException e) {
+      this.maxMemory = 100 * 1024 * 1024;
+      this.maxMemoryPolicy = MaxMemoryPolicy.ALLKEYSLRU;
+      this.canPolicyBeChanged = true;
+    }
 
     createCacheStorages();
 
-    // Запускаем периодическую очистку просроченных ключей
     this.cleanupScheduler = Executors.newSingleThreadScheduledExecutor();
-    this.cleanupScheduler.scheduleAtFixedRate(this::cleanupDatabase,
-        1, 1, TimeUnit.MINUTES);
-    this.canPolicyBeChanged = true;
+    this.cleanupScheduler.scheduleAtFixedRate(
+        this::cleanupDatabase,
+        1, 1, TimeUnit.MINUTES
+    );
   }
 
-  // Основные операции (работают с единственной БД)
+  // Основные операции
   public CacheResult post(String key, Object value, DataType dataType,
       String user, Long ttlSeconds, long sizeBytes) {
     if (canPolicyBeChanged) {
@@ -93,8 +98,31 @@ public class CacheStorageService {
     return CacheResult.error("Can't change configuration");
   }
 
+  private Properties loadProperties() throws IOException {
+    Properties props = new Properties();
+    try (InputStream input = getClass().getClassLoader()
+        .getResourceAsStream("application.properties")) {
+      if (input != null) {
+        props.load(input);
+      }
+    }
+    return props;
+  }
+
+  private MaxMemoryPolicy parsePolicy(Properties props, String key, MaxMemoryPolicy defaultValue) {
+    String value = props.getProperty(key);
+    if (value != null) {
+      try {
+        return MaxMemoryPolicy.valueOf(value.toUpperCase());
+      } catch (IllegalArgumentException e) {
+        // Оставляем defaultValue
+      }
+    }
+    return defaultValue;
+  }
+
   private void createCacheStorages() {
-      database = new CacheStorage(maxMemory, maxMemoryPolicy);
+    database = new CacheStorage(maxMemory, maxMemoryPolicy);
   }
 
   public long getmaxMemory() {
