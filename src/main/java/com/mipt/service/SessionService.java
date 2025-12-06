203ece48da68d72f177e4a23eaef6d3f5709a07a
Sharipov Razil <sharipov.rr@phystech.edu>
2025-12-07 01:24:06 +0300

    исправлены некоторые ошибки

--- src/main/java/com/mipt/service/SessionService.java
+++ src/main/java/com/mipt/service/SessionService.java
@@ -76,9 +76,9 @@ public class SessionService {
     Session session = activeSessions.get(sessionToken);
     if (session != null) {
       long ttl = sessionTtlHours * 60;
-      return !session.isValid(ttl);
+      return session.isValid(ttl);
     }
-    return true;
+    return false;
   }
 
   /**
